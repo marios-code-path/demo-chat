752c9804e35fe025a067c45293be0b997941284c
Mario Gray <mario.gray@gmail.com>
2019-07-22 19:02:09 -0700

    consul

--- chat-service-rsocket/src/main/kotlin/com/demo/chat/ChatServiceRsocketApplication.kt
+++ chat-service-rsocket/src/main/kotlin/com/demo/chat/ChatServiceRsocketApplication.kt
@@ -1,8 +1,11 @@
 package com.demo.chat
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.FilterType
 
 @SpringBootApplication
@@ -10,6 +13,12 @@ import org.springframework.context.annotation.FilterType
     ComponentScan.Filter(type = FilterType.ANNOTATION, value = [ExcludeFromTests::class])
 ])
 class ChatServiceRsocketApplication
+
+@EnableConfigurationProperties
+@EnableAutoConfiguration
+@Configuration
+@ExcludeFromTests
+class ServiceDiscoveryConfiguration
 
 fun main(args: Array<String>) {
     runApplication<ChatServiceRsocketApplication>(*args)
