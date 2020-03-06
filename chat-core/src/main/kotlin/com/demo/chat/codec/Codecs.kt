ad1ba660f6a20ce9293bb173d8b96afbb157089c
Mario Gray <mario.gray@gmail.com>
2020-03-06 15:35:37 -0800

    notes

--- chat-core/src/main/kotlin/com/demo/chat/codec/Codecs.kt
+++ chat-core/src/main/kotlin/com/demo/chat/codec/Codecs.kt
@@ -1,6 +1,5 @@
 package com.demo.chat.codec
 
-import com.demo.chat.domain.Key
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.JsonNodeType
@@ -11,49 +10,43 @@ interface Codec<F, E> {
     fun decode(record: F): E
 }
 
-// TODO IN PRODUCTION: We are getting Base64-encoded?!?!?
+// TODO: Fix this to automatically decode CBOR-encoded data
+// TODO: Looks like a UUID/CBOR decoding/encoding issue
 object JsonNodeAnyCodec : Codec<JsonNode, Any> {
-    override fun decode(record: JsonNode): Any {
-        println("Record: ${record}")
-        println("PROPERTY: ${record.nodeType}")
-        return when (record.nodeType) {
-            JsonNodeType.BINARY -> {
-                val f = CBORFactory()
-                val mapper = ObjectMapper(f)
-                val cborData = mapper.writeValueAsBytes(record.binaryValue())
-                println("CBOR: $cborData")
+    override fun decode(record: JsonNode): Any = when (record.nodeType) {
+        JsonNodeType.BINARY -> {
+            val f = CBORFactory()
+            val mapper = ObjectMapper(f)
+            val cborData = mapper.writeValueAsBytes(record.binaryValue())
 
-                 val data = mapper.readValue(cborData, UUID::class.java)
-                println("DATA: $data")
+            val data = mapper.readValue(cborData, UUID::class.java)
 
-                data
-
-            }
-            JsonNodeType.NUMBER -> record.asLong()
-            else -> {
-                try {
-                    UUID.fromString(record.asText())
-                } catch (e: Exception) {
-                    record.asText()
-                }
+            data
+        }
+        JsonNodeType.NUMBER -> record.asLong()
+        else -> {
+            try {
+                UUID.fromString(record.asText())
+            } catch (e: Exception) {
+                record.asText()
             }
         }
     }
 }
 
-class EmptyStringCodec: Codec<Unit, String> {
+class EmptyStringCodec : Codec<Unit, String> {
     override fun decode(record: Unit): String {
         return ""
     }
 }
 
-class EmptyNumberCodec: Codec<Unit, Number> {
+class EmptyNumberCodec : Codec<Unit, Number> {
     override fun decode(record: Unit): Number {
         return 0
     }
 }
 
-class EmptyUUIDCodec: Codec<Unit, UUID> {
+class EmptyUUIDCodec : Codec<Unit, UUID> {
     override fun decode(record: Unit): UUID {
         return UUID(0L, 0L)
     }
