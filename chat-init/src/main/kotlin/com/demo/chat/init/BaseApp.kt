e815adbf794d91486a98a0b59b4252c4fa9818b2
Mario Gray <mario.gray@gmail.com>
2023-02-15 17:10:22 -0800

    autoconfiguration issue update ; move import to local profile mains

--- chat-init/src/main/kotlin/com/demo/chat/init/BaseApp.kt
+++ chat-init/src/main/kotlin/com/demo/chat/init/BaseApp.kt
@@ -1,45 +1,12 @@
 package com.demo.chat.init
 
-import com.demo.chat.client.rsocket.DefaultRequesterFactory
-import com.demo.chat.config.TypeUtilConfiguration
-import com.demo.chat.config.client.rsocket.ClientConfiguration
-import com.demo.chat.config.client.rsocket.CompositeClientsConfiguration
-import com.demo.chat.config.client.rsocket.CoreClientsConfiguration
 import com.demo.chat.config.client.rsocket.RSocketClientProperties
-import com.demo.chat.config.persistence.memory.KeyGenConfiguration
-import com.demo.chat.config.secure.AuthConfiguration
-import com.demo.chat.config.secure.TransportConfiguration
-import com.demo.chat.domain.serializers.DefaultChatJacksonModules
 import com.demo.chat.init.domain.BootstrapProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration
-import org.springframework.boot.autoconfigure.rsocket.RSocketMessagingAutoConfiguration
-import org.springframework.boot.autoconfigure.rsocket.RSocketStrategiesAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication(scanBasePackages = ["com.demo.chat.config"])
-@EnableConfigurationProperties(BootstrapProperties::class, RSocketClientProperties::class)
-@Import(
-    // Serialization
-    JacksonAutoConfiguration::class,
-    DefaultChatJacksonModules::class,
-    RSocketStrategiesAutoConfiguration::class,
-    RSocketMessagingAutoConfiguration::class,
-    // TYPES
-    TypeUtilConfiguration::class,
-    // Transport Security
-    TransportConfiguration::class,
-    // Services
-    KeyGenConfiguration::class,
-    DefaultRequesterFactory::class,
-    ClientConfiguration::class,
-    CoreClientsConfiguration::class,
-    CompositeClientsConfiguration::class,
-    AuthConfiguration::class,
-    InitApp::class
-)
 class BaseApp {
 
     companion object {
--- chat-init/src/main/kotlin/com/demo/chat/init/InitApp.kt
+++ chat-init/src/main/kotlin/com/demo/chat/init/InitApp.kt
@@ -1,10 +1,25 @@
 package com.demo.chat.init
 
+import com.demo.chat.client.rsocket.DefaultRequesterFactory
+import com.demo.chat.config.TypeUtilConfiguration
+import com.demo.chat.config.client.rsocket.ClientConfiguration
+import com.demo.chat.config.client.rsocket.CompositeClientsConfiguration
+import com.demo.chat.config.client.rsocket.CoreClientsConfiguration
+import com.demo.chat.config.client.rsocket.RSocketClientProperties
+import com.demo.chat.config.persistence.memory.KeyGenConfiguration
+import com.demo.chat.config.secure.AuthConfiguration
+import com.demo.chat.config.secure.TransportConfiguration
 import com.demo.chat.domain.*
+import com.demo.chat.domain.serializers.DefaultChatJacksonModules
 import com.demo.chat.init.domain.BootstrapProperties
 import com.demo.chat.service.composite.ChatUserService
 import com.demo.chat.service.security.AuthorizationService
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.AutoConfigureAfter
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration
+import org.springframework.boot.autoconfigure.rsocket.RSocketMessagingAutoConfiguration
+import org.springframework.boot.autoconfigure.rsocket.RSocketStrategiesAutoConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.*
 import reactor.core.publisher.Mono
 
@@ -14,6 +29,25 @@ import reactor.core.publisher.Mono
  */
 @Profile("init")
 @Configuration
+@EnableConfigurationProperties(BootstrapProperties::class, RSocketClientProperties::class)
+@Import(
+    // Serialization
+    JacksonAutoConfiguration::class,
+    DefaultChatJacksonModules::class,
+    RSocketStrategiesAutoConfiguration::class,
+    RSocketMessagingAutoConfiguration::class,
+    // TYPES
+    TypeUtilConfiguration::class,
+    // Transport Security
+    TransportConfiguration::class,
+    // Services
+    KeyGenConfiguration::class,
+    DefaultRequesterFactory::class,
+    ClientConfiguration::class,
+    CoreClientsConfiguration::class,
+    CompositeClientsConfiguration::class,
+    AuthConfiguration::class
+)
 class InitApp{
 
     // TODO: NOTE - the anonymous key used here should be ignored.
