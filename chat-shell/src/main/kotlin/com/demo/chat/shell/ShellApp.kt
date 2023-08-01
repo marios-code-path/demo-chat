84ebbd3e9eae4577ce48ec7704f7a47ebc397118
Mario Gray <mario.gray@gmail.com>
2023-07-31 23:27:59 -0700

    adds some k8s deployments of cassandra

--- chat-shell/src/main/kotlin/com/demo/chat/shell/ShellApp.kt
+++ chat-shell/src/main/kotlin/com/demo/chat/shell/ShellApp.kt
@@ -3,6 +3,7 @@ package com.demo.chat.shell
 import com.demo.chat.client.rsocket.RequestMetadata
 import com.demo.chat.domain.knownkey.Anon
 import io.rsocket.metadata.WellKnownMimeType
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -32,6 +33,9 @@ class ShellApp {
 @Configuration
 class ShellStateConfiguration {
 
+    @Value("\${app.shell.anonymous.password:_}")
+    private lateinit var anonymousPassword: String
+
     companion object {
         var loggedInUser: Optional<Any> = Optional.empty()
         var loginMetadata: Optional<UsernamePasswordMetadata> = Optional.empty()
@@ -42,7 +46,7 @@ class ShellStateConfiguration {
         val metadata = RequestMetadata(
             loginMetadata
                 .map { UsernamePasswordMetadata(it.username, it.password) }
-                .orElseGet { UsernamePasswordMetadata(Anon::class.java.simpleName, "_") },
+                .orElseGet { UsernamePasswordMetadata(Anon::class.java.simpleName, anonymousPassword) },
             MimeTypeUtils.parseMimeType(WellKnownMimeType.MESSAGE_RSOCKET_AUTHENTICATION.string)
         )
         metadata
