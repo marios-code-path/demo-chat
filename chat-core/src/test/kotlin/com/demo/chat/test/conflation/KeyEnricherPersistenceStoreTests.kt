9d0fbde8b010d0a719397f557f6bb4ce7e0223f5
Mario Gray <mario.gray@gmail.com>
2024-03-19 01:53:49 -0400

    fixes tests

--- chat-core/src/test/kotlin/com/demo/chat/test/conflation/KeyEnricherPersistenceStoreTests.kt
+++ chat-core/src/test/kotlin/com/demo/chat/test/conflation/KeyEnricherPersistenceStoreTests.kt
@@ -3,11 +3,15 @@ package com.demo.chat.test.conflation
 import com.demo.chat.domain.User
 import com.demo.chat.service.conflate.KeyEnricherPersistenceStore
 import com.demo.chat.test.TestUserSupplier
+import com.demo.chat.test.index.MockIndexResolver
+import com.demo.chat.test.persistence.MockPersistenceResolver
 import com.demo.chat.test.persistence.PersistenceTestBase
 import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.extension.ExtendWith
 
 data class TestUserRequest(val name: String, val handle: String, val uri: String)
 
-@Disabled
+//@Disabled
+@ExtendWith(MockPersistenceResolver::class, MockIndexResolver::class)
 class KeyEnricherPersistenceStoreTests(store: KeyEnricherPersistenceStore<String, TestUserRequest, User<String>>)
     : PersistenceTestBase<String, User<String>>(TestUserSupplier, store)
---
+++ shell-scripts/docker-create-pks-vol.sh
@@ -0,0 +1,3 @@
+. ./key_functions.sh
+export VOLUME_NAME=chat-pks
+docker_volume_gen
